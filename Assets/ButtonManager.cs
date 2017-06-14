12b5112888117da04d771e6233f7fb84f3f24a1a
aleaiudi <aleaiudi1@hotmail.it>
2017-06-14 19:27:17 +0200

    added script

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+
+    private byte[] Action;
+
+    private byte[] packet;
+
+    private string deviceName;
+
+
+	// Use this for initialization
+	void Start () {
+        Action = new byte[] { 0, 1, 2, 3 };
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
