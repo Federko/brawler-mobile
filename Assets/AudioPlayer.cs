b98163479aecc7f5dd863b27203bf3f7043379fb
Federico Cardetti <federko996@gmail.com>
2017-06-16 11:55:18 +0200

    Added chance to register and play audio from the device

---
+++ Assets/AudioPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+    private AudioSource audio;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void PlayAudio()
+    {
+        if(audio.clip!=null)
+        {
+            audio.Play();
+        }
+    }
+}
---
+++ Assets/AudioRegister.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioRegister : MonoBehaviour
+{
+    public AudioSource audio;
+
+    private AudioClip clip;
+    private Button button;
+    ColorBlock defaultColors;
+    ColorBlock pressedColors;
+
+    string[] microphones;
+    // Use this for initialization
+    void Start()
+    {
+        button = GetComponent<Button>();
+        defaultColors = button.colors;
+        pressedColors = ColorBlock.defaultColorBlock;
+        pressedColors.normalColor = defaultColors.pressedColor;
+        pressedColors.highlightedColor = defaultColors.pressedColor;
+        pressedColors.pressedColor = defaultColors.normalColor;
+
+        microphones = Microphone.devices;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void RegisterAudio()
+    {
+        clip = Microphone.Start(microphones[0], false, 10, 44100);
+        button.colors = pressedColors;      
+    }
+    private void StopRegisterAudio()
+    {
+        Microphone.End(microphones[0]);
+        button.colors = defaultColors;
+        audio.clip = clip;
+    }
+    public void HandleAudio()
+    {
+        if (Microphone.IsRecording(microphones[0]))
+            StopRegisterAudio();
+        else
+            RegisterAudio();
+    }
+}
