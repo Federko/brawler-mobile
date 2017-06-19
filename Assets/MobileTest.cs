6442b23f8ce20e155891a566669984a7a4e8cb4b
Federico Cardetti <federko996@gmail.com>
2017-06-19 17:17:18 +0200

    created new branch for graphic

---
+++ Assets/MobileTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class MobileTest : MonoBehaviour
+{
+    public static string DeviceUniqueIdentifier;
+    public Text text;
+    // Use this for initialization
+    void Start()
+    {
+        DeviceUniqueIdentifier = SystemInfo.deviceUniqueIdentifier;
+        text = GetComponentInChildren<Text>();
+        text.text = DeviceUniqueIdentifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
