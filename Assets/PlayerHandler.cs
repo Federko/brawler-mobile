86cb77d714ae31196f94f567bc41e57f4a413470
Federico Cardetti <federko996@gmail.com>
2017-06-19 20:01:46 +0200

    implemented UI

--- Assets/PlayerHandler.cs
+++ Assets/PlayerHandler.cs
@@ -3,30 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerHandler : MonoBehaviour {
+public class PlayerHandler : MonoBehaviour
+{
     public GameObject playerPrefab;
-    List<GameObject> players;
-
-	// Use this for initialization
-	void Start () {
-        players = new List<GameObject>();
+    public Text PlayersNumber;
+    // Use this for initialization
+    void Start()
+    {
         for (int i = 0; i < 8; i++)
         {
             IstantiatePlayer(playerPrefab);
-        } 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetKeyDown(KeyCode.Return))
             IstantiatePlayer(playerPrefab);
-	}
+    }
     void IstantiatePlayer(GameObject playerPrefab)
     {
         GameObject player = GameObject.Instantiate<GameObject>(playerPrefab);
-        player.GetComponent<LayoutElement>().preferredHeight = Screen.height *0.115f;
-        player.GetComponent<LayoutElement>().preferredWidth = Screen.width;
         player.transform.SetParent(transform);
-        players.Add(player);
+        player.transform.localScale = Vector3.one;
+        PlayersNumber.text = "Players:\n" + transform.childCount + "/8";
     }
 }
