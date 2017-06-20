b088091780dcdf0b71bfac8141ebcfa4fd78ae3f
Federico Cardetti <federko996@gmail.com>
2017-06-20 12:42:55 +0200

    added script to close PanelAudio

---
+++ Assets/DisablePanelAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisablePanelAudio : MonoBehaviour {
+    public Image recordBar;
+    public Image maxRecordBar;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void DisableWindow()
+    {
+        recordBar.fillAmount = 0f;
+        recordBar.gameObject.SetActive(false);
+        maxRecordBar.gameObject.SetActive(false);
+        gameObject.SetActive(false);
+    }
+}
