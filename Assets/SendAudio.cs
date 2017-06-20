847fe8a47a3fa8287a1d3165145553b19dcac5f7
Federico Cardetti <federko996@gmail.com>
2017-06-20 12:41:06 +0200

    added new script to send to the server audio messages

---
+++ Assets/SendAudio.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class SendAudio : MonoBehaviour
+{
+    private UnityWebRequest request;
+    private AsyncOperation operation;
+
+    private AudioClip clip;
+
+    private string url;
+    // Use this for initialization
+    void Start()
+    {
+        url = "http://taiga.aiv01.it/mobile/match/audio/";
+
+        clip = transform.parent.GetComponentInChildren<AudioRegister>().Clip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (operation != null && operation.isDone)
+        {
+            JsonResponse response = JsonUtility.FromJson<JsonResponse>(request.downloadHandler.text);
+
+            if (response.mobile_upload_audio)
+                Debug.Log("Uploaded");
+            else
+                Debug.Log("upload failed");
+        }
+    }
+    public void SendAudioToServer()
+    {
+        if (clip == null)
+        {
+            Debug.Log("Nothing to send!");
+            return;
+        }
+
+        float[] array = new float[clip.channels * clip.samples];
+        if (clip.GetData(array, 0))
+        {
+            byte[] rawAudio = new byte[array.Length * sizeof(float)];
+            Buffer.BlockCopy(array, 0, rawAudio, 0, array.Length);
+            string formData = BitConverter.ToString(rawAudio);
+            request = UnityWebRequest.Post(url + "?mobile_id=" + SystemInfo.deviceUniqueIdentifier, formData);
+            UploadHandlerRaw upHandler = new UploadHandlerRaw(rawAudio);
+            upHandler.contentType = "Application/octet-stream";
+            request.uploadHandler = upHandler;
+            operation = request.Send();
+            return;
+        }
+        Debug.Log("ERROR! IMPOSSIBLE GET RAW AUDIO");
+    }
+    public class JsonResponse
+    {
+        public bool mobile_upload_audio;
+    }
+}
