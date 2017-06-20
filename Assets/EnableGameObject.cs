1fb386a0db51954bfb0e50884185005542988f88
Federico Cardetti <federko996@gmail.com>
2017-06-20 12:24:19 +0200

    Added EnableGameObject script for enable/disable a specific gameobject

---
+++ Assets/EnableGameObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableGameObject : MonoBehaviour
+{
+    public GameObject target;
+
+    public void Enable()
+    {
+        if (target.activeInHierarchy)
+            target.SetActive(false);
+        else
+            target.SetActive(true);
+    }
+}
